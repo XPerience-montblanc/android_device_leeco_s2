9b6cb5054976aabffe7979a11e57b9f255d50b5e
13THWARRIOR <13THWARRIOR@users.noreply.github.com>
2016-12-04 00:20:21 +0530

    initial commit

--- cm.mk
+++ cm.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit from kenzo device
+# Inherit from s2 device
 $(call inherit-product, device/leeco/s2/device.mk)
 
 # Set those variables here to overwrite the inherited values.
